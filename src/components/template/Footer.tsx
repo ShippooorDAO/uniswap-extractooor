0c0408be6c3551fc6a6a84d1863268eb6894d524
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2022-10-13 09:56:44 -0400

    Change footer link

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -1,11 +1,7 @@
 const Footer = () => {
   return (
     <footer className="drop-shadow-md flex items-center justify-center h-28">
-      <a
-        className="text-md"
-        href="https://twitter.com/shippooor"
-        target="_blank"
-      >
+      <a className="text-md" href="https://shippooor.xyz/" target="_blank">
         &#128674; Shippoooor
       </a>
     </footer>
